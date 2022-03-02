e904700cce0e63a6a3512af3c4e1e745001b08f2
MentalCoder91 <anish.alwekar@gmail.com>
2022-03-02 15:05:57 -0800

    commit-4

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from 'src/app/Service/backend.service';
 import { Question } from 'src/app/Model/Question';
-import { FormArray, FormGroup, FormControl } from '@angular/forms';
+import { FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-question-list',
@@ -19,40 +19,40 @@ export class QuestionListComponent implements OnInit {
     this.backendService.getQuestions().subscribe(
       (response) => {
         //Next callback
-        console.log('response received');
+        //console.log('response received');
         //console.log(response);
         this.questionList = response;
+        for (let x in this.questionList) {
+          console.log(this.questionList[x].options);
+          (<FormArray>this.checkBoxForm.get('questionsArray')).push(
+            new FormGroup({
+              qname: new FormControl(this.questionList[x].qname),
+              options: new FormArray(
+                this.loadOptions(this.questionList[x].options)
+              ),
+            })
+          );
+        }
       },
       (error) => {
         //Error callback
-        console.error('Request failed with error');
+        //console.error('Request failed with error');
         alert(error);
       },
       () => {
         //Complete callback
-        console.log('Request completed');
+        //console.log('Request completed');
       }
     );
-
-    for (let x in this.questionList) {
-      //console.log(this.questionList[x].qname);
-      (<FormArray>this.checkBoxForm.get('questionsArray')).push(
-        new FormGroup({
-          qname: new FormControl(this.questionList[x].qname),
-          options: new FormArray(
-            this.loadOptions(this.questionList[x].options)
-          ),
-        })
-      );
-    }
   }
 
   loadOptions(arr) {
     let controls = [];
     for (let x in arr) {
+      console.log(arr[x].optValue);
       controls.push(
         new FormGroup({
-          select: new FormControl(arr[x].OptValue),
+          select: new FormControl(arr[x].optValue),
         })
       );
     }
@@ -65,6 +65,12 @@ export class QuestionListComponent implements OnInit {
   }
 
   returnQuestionsArray() {
+    //console.log(this.checkBoxForm.get('questionsArray')['controls']);
     return this.checkBoxForm.get('questionsArray')['controls'];
+  }
+
+  returnOptions(x) {
+    console.log(x.get('options').controls);
+    return x.get('options').controls;
   }
 }
